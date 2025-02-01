7e470366e7316241d0b050eca2f1eeed8911fc1a
Abdul <abdulhaleemtamimi@gmail.com>
2025-02-01 07:25:26 +0300

    Non-Root

--- app/src/main/java/ps/reso/instaeclipse/Xposed/Module.java
+++ app/src/main/java/ps/reso/instaeclipse/Xposed/Module.java
@@ -24,7 +24,6 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import ps.reso.instaeclipse.mods.DevOptionsEnable;
 import ps.reso.instaeclipse.mods.GhostModeDM;
 import ps.reso.instaeclipse.mods.Interceptor;
-import ps.reso.instaeclipse.mods.misc.AutoPlayDisable;
 import ps.reso.instaeclipse.mods.misc.StoryFlipping;
 
 
@@ -33,26 +32,10 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     private static final String IG_PACKAGE_NAME = "com.instagram.android";
     private static final String MY_PACKAGE_NAME = "ps.reso.instaeclipse";
     public static DexKitBridge dexKitBridge;
+    public static ClassLoader hostClassLoader;
     private static String moduleSourceDir;
     private static String moduleLibDir;
     List<Predicate<URI>> uriConditions = new ArrayList<>();
-    Boolean isDevEnabled;
-    Boolean isGhost_Enabled;
-    Boolean isGhost_DM_Enabled;
-    Boolean isGhost_Story_Enabled;
-    Boolean isGhost_Live_Enabled;
-    Boolean isDistraction_Free_Enabled;
-    Boolean isDistraction_Stories_Enabled;
-    Boolean isDistraction_Feed_Enabled;
-    Boolean isDistraction_Reels_Enabled;
-    Boolean isDistraction_Explore_Enabled;
-    Boolean isDistraction_Comments_Enabled;
-    Boolean isRemove_Ads_Enabled;
-    Boolean isRemove_Analytics_Enabled;
-    Boolean is_Misc_Enabled;
-    Boolean isStop_Story_Flipping_Enabled;
-    Boolean isStop_Video_AutoPlay_Enabled;
-    public static ClassLoader hostClassLoader;
 
     // for dev usage
     public static void showToast(final String text) {
@@ -72,59 +55,8 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
         // XposedBridge.log("InstaEclipse | Module paths initialized:" + "\nSourceDir: " + moduleSourceDir + "\nLibDir: " + moduleLibDir);
     }
 
-    public void loadPreferences() {
-        try {
-            // Load & Reload preferences
-            XposedPreferences.loadPreferences();
-            XposedPreferences.reloadPrefs();
-
-            // Developer options
-            isDevEnabled = XposedPreferences.getPrefs().getBoolean("enableDev", false);
-
-            // Ghost mode
-            isGhost_Enabled = XposedPreferences.getPrefs().getBoolean("enableGhostMode", false);
-
-            if (isGhost_Enabled) {
-                isGhost_DM_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeDM", false);
-                isGhost_Story_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeLive", false);
-                isGhost_Live_Enabled = XposedPreferences.getPrefs().getBoolean("ghostModeStory", false);
-            }
-
-            // Distraction free
-            isDistraction_Free_Enabled = XposedPreferences.getPrefs().getBoolean("enableDistractionFree", false);
-
-            if (isDistraction_Free_Enabled) {
-                isDistraction_Stories_Enabled = XposedPreferences.getPrefs().getBoolean("disableStories", false);
-                isDistraction_Feed_Enabled = XposedPreferences.getPrefs().getBoolean("disableFeed", false);
-                isDistraction_Reels_Enabled = XposedPreferences.getPrefs().getBoolean("disableReels", false);
-                isDistraction_Explore_Enabled = XposedPreferences.getPrefs().getBoolean("disableExplore", false);
-                isDistraction_Comments_Enabled = XposedPreferences.getPrefs().getBoolean("disableComments", false);
-            }
-
-            // Remove ads
-            isRemove_Ads_Enabled = XposedPreferences.getPrefs().getBoolean("removeAds", false);
-
-            // Remove analysis
-            isRemove_Analytics_Enabled = XposedPreferences.getPrefs().getBoolean("removeAnalytics", false);
-
-            // Misc options
-            is_Misc_Enabled = XposedPreferences.getPrefs().getBoolean("miscOptions", false);
-
-            if (is_Misc_Enabled){
-                isStop_Story_Flipping_Enabled = XposedPreferences.getPrefs().getBoolean("storyFlipping", false);
-                isStop_Video_AutoPlay_Enabled = XposedPreferences.getPrefs().getBoolean("videoAutoPlay", false);
-            }
-
-
-        } catch (Exception e) {
-            XposedBridge.log("(InstaEclipse): Failed to initialize preferences: " + e.getMessage());
-        }
-    }
-
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
-        // Ensure preferences are loaded
-        loadPreferences();
 
         XposedBridge.log("(InstaEclipse): Loaded package: " + lpparam.packageName);
 
@@ -191,73 +123,32 @@ public class Module implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 
             Interceptor interceptor = new Interceptor();
 
-            if (isDevEnabled) {
-                DevOptionsEnable devOptionsEnable = new DevOptionsEnable();
-                devOptionsEnable.handleDevOptions(dexKitBridge);
-            }
+            DevOptionsEnable devOptionsEnable = new DevOptionsEnable();
+            devOptionsEnable.handleDevOptions(dexKitBridge);
 
-            if (isGhost_Enabled) {
-                if (isGhost_DM_Enabled) {
-                    GhostModeDM ghostModeDM = new GhostModeDM();
-                    ghostModeDM.handleGhostMode(lpparam);
-                }
 
-                if (isGhost_Story_Enabled) {
-                    uriConditions.add(uri -> uri.getPath().contains("/api/v2/media/seen/"));
-                }
+            GhostModeDM ghostModeDM = new GhostModeDM();
+            ghostModeDM.handleGhostMode(lpparam);
 
-                if (isGhost_Live_Enabled) {
-                    uriConditions.add(uri -> uri.getPath().contains("/heartbeat_and_get_viewer_count/"));
-                }
-            }
+            uriConditions.add(uri -> uri.getPath().contains("/api/v2/media/seen/"));
 
-            if (isDistraction_Free_Enabled) {
-                if (isDistraction_Stories_Enabled) {
-                    uriConditions.add(uri -> uri.getPath().contains("/feed/reels_tray/") || uri.getPath().contains("/api/v1/feed/reels_media_stream/"));
-                    uriConditions.add(uri -> uri.getPath().contains("feed/get_latest_reel_media/"));
-                    uriConditions.add(uri -> uri.getPath().contains("direct_v2/pending_inbox/?visual_message"));
-                    uriConditions.add(uri -> uri.getPath().contains("stories/hallpass/"));
-                }
-                if (isDistraction_Feed_Enabled) {
-                    uriConditions.add(uri -> uri.getPath().endsWith("/feed/timeline/"));
 
-                }
-                if (isDistraction_Reels_Enabled) {
-                    uriConditions.add(uri -> ((uri.getPath().endsWith("/qp/batch_fetch/") || uri.getPath().contains("api/v1/clips") || uri.getPath().contains("clips") || uri.getPath().contains("mixed_media") || uri.getPath().contains("mixed_media/discover/stream/"))));
-                }
-                if (isDistraction_Explore_Enabled) {
-                    uriConditions.add(uri -> ((uri.getPath().contains("/discover/topical_explore") || uri.getPath().contains("/discover/topical_explore_stream"))));
-                    uriConditions.add(uri -> (uri.getHost().contains("i.instagram.com")) && (uri.getPath().contains("/api/v1/fbsearch/top_serp/")));
-                }
-                if (isDistraction_Comments_Enabled) {
-                    uriConditions.add(uri -> ((uri.getPath().contains("/api/v1/media/") && uri.getPath().contains("comments/"))));
-                }
-            }
+            uriConditions.add(uri -> uri.getPath().contains("/heartbeat_and_get_viewer_count/"));
 
-            if (isRemove_Ads_Enabled) {
-                uriConditions.add(uri -> uri.getPath().contains("profile_ads/get_profile_ads/"));
-                uriConditions.add(uri -> uri.getPath().contains("/ads/"));
-                uriConditions.add(uri -> uri.getPath().contains("/feed/injected_reels_media/"));
-                uriConditions.add(uri -> uri.getPath().equals("/api/v1/ads/graphql/"));
-            }
+            uriConditions.add(uri -> uri.getPath().contains("profile_ads/get_profile_ads/"));
+            uriConditions.add(uri -> uri.getPath().contains("/ads/"));
+            uriConditions.add(uri -> uri.getPath().contains("/feed/injected_reels_media/"));
+            uriConditions.add(uri -> uri.getPath().equals("/api/v1/ads/graphql/"));
 
-            if (isRemove_Analytics_Enabled) {
-                uriConditions.add(uri -> uri.getHost().contains("graph.instagram.com"));
-                uriConditions.add(uri -> uri.getHost().contains("graph.facebook.com"));
-                uriConditions.add(uri -> uri.getPath().contains("/logging_client_events"));
-                uriConditions.add(uri -> uri.getPath().endsWith("/activities"));
-            }
 
-            if (is_Misc_Enabled){
-                if (isStop_Story_Flipping_Enabled){
-                    StoryFlipping storyFlipping = new StoryFlipping();
-                    storyFlipping.handleStoryFlippingDisable(dexKitBridge);
-                }
-                if (isStop_Video_AutoPlay_Enabled){
-                    AutoPlayDisable autoPlayDisable = new AutoPlayDisable();
-                    autoPlayDisable.handleAutoPlayDisable(dexKitBridge);
-                }
-            }
+            uriConditions.add(uri -> uri.getHost().contains("graph.instagram.com"));
+            uriConditions.add(uri -> uri.getHost().contains("graph.facebook.com"));
+            uriConditions.add(uri -> uri.getPath().contains("/logging_client_events"));
+            uriConditions.add(uri -> uri.getPath().endsWith("/activities"));
+
+            StoryFlipping storyFlipping = new StoryFlipping();
+            storyFlipping.handleStoryFlippingDisable(dexKitBridge);
+
 
             // Pass the dynamically rebuilt conditions to the interceptor
             if (!uriConditions.isEmpty()) {
